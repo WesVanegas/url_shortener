e3ae3ea3051f07f7cfcb3085bc45a400a5b57c32
Wes Vanegas <wesleyvanegas13@gmail.com>
2024-09-10 17:52:00 -0500

    Add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from hashids import Hashids
+import datetime
+
 
 # Create your models here.
+class Link(models.Model):
+    url = models.URLField()
+    code = models.CharField(max_length=8, blank=True)
+    date = models.DateField(auto_now_add=True)
+    count = models.PositiveIntegerField(default=0)
