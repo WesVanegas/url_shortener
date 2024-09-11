3251f3a3ec0d47fab6676915a2eca651bc500a56
Wes Vanegas <wesleyvanegas13@gmail.com>
2024-09-11 11:28:16 -0500

    Update models

--- core/models.py
+++ core/models.py
@@ -10,3 +10,18 @@ class Link(models.Model):
     code = models.CharField(max_length=8, blank=True)
     date = models.DateField(auto_now_add=True)
     count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Links'
+
+    def __str__(self):
+        return f"URL: {self.url} Codigo: {self.code}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.code:
+            self.code = Hashids(min_length=4, alphabet='abcdefghijklmnopqrstuvxyz').encode(self.pk)
+            self.save()
+
+    def get_absolute_url(self):
+        return reverse("core:detail", kwargs={"pk": self.pk})
